63694484b06dd6da8695845ad6e67fd18d6ae347
Gyumin Sim <sim0629@gmail.com>
2015-04-12 02:34:52 +0900

    [64x64] misses: 1347 (non-diagonal xor)

--- trans.c
+++ trans.c
@@ -56,54 +56,176 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
         {
             for (ii = 0; ii < N; ii += 8)
             {
-                j = jj;
-                for (i = ii; i < ii + 4; i++)
+                if (ii == jj)
                 {
-                    a0 = A[i][j + 0];
-                    a1 = A[i][j + 1];
-                    a2 = A[i][j + 2];
-                    a3 = A[i][j + 3];
-                    a4 = A[i][j + 4];
-                    a5 = A[i][j + 5];
-                    a6 = A[i][j + 6];
-                    a7 = A[i][j + 7];
-                    B[j + 0][i] = a0;
-                    B[j + 1][i] = a1;
-                    B[j + 2][i] = a2;
-                    B[j + 3][i] = a3;
-                    B[j + 3][i + 4] = a4;
-                    B[j + 2][i + 4] = a5;
-                    B[j + 1][i + 4] = a6;
-                    B[j + 0][i + 4] = a7;
-                }
-                for (i = ii + 4; i < ii + 8; i++)
-                {
-                    a0 = A[i][j + 0];
-                    a1 = A[i][j + 1];
-                    a2 = A[i][j + 2];
-                    a3 = A[i][j + 3];
-                    a4 = A[i][j + 4];
-                    a5 = A[i][j + 5];
-                    a6 = A[i][j + 6];
-                    a7 = A[i][j + 7];
-                    B[j + 7][i - 4] = a0;
-                    B[j + 6][i - 4] = a1;
-                    B[j + 5][i - 4] = a2;
-                    B[j + 4][i - 4] = a3;
-                    B[j + 4][i] = a4;
-                    B[j + 5][i] = a5;
-                    B[j + 6][i] = a6;
-                    B[j + 7][i] = a7;
-                }
-                for (j = 0; j < 4; j++)
-                {
-                    for (i = 0; i < 4; i++)
+                    j = jj;
+                    for (i = ii; i < ii + 4; i++)
                     {
-                        a0 = B[jj + j][ii + 4 + i];
-                        a1 = B[jj + 4 + (3 - j)][ii + i];
-                        B[jj + j][ii + 4 + i] = a1;
-                        B[jj + 4 + (3 - j)][ii + i] = a0;
+                        a0 = A[i][j + 0];
+                        a1 = A[i][j + 1];
+                        a2 = A[i][j + 2];
+                        a3 = A[i][j + 3];
+                        a4 = A[i][j + 4];
+                        a5 = A[i][j + 5];
+                        a6 = A[i][j + 6];
+                        a7 = A[i][j + 7];
+                        B[j + 0][i] = a0;
+                        B[j + 1][i] = a1;
+                        B[j + 2][i] = a2;
+                        B[j + 3][i] = a3;
+                        B[j + 3][i + 4] = a4;
+                        B[j + 2][i + 4] = a5;
+                        B[j + 1][i + 4] = a6;
+                        B[j + 0][i + 4] = a7;
                     }
+                    for (i = ii + 4; i < ii + 8; i++)
+                    {
+                        a0 = A[i][j + 0];
+                        a1 = A[i][j + 1];
+                        a2 = A[i][j + 2];
+                        a3 = A[i][j + 3];
+                        a4 = A[i][j + 4];
+                        a5 = A[i][j + 5];
+                        a6 = A[i][j + 6];
+                        a7 = A[i][j + 7];
+                        B[j + 7][i - 4] = a0;
+                        B[j + 6][i - 4] = a1;
+                        B[j + 5][i - 4] = a2;
+                        B[j + 4][i - 4] = a3;
+                        B[j + 4][i] = a4;
+                        B[j + 5][i] = a5;
+                        B[j + 6][i] = a6;
+                        B[j + 7][i] = a7;
+                    }
+                    for (j = 0; j < 4; j++)
+                    {
+                        for (i = 0; i < 4; i++)
+                        {
+                            a0 = B[jj + j][ii + 4 + i];
+                            a1 = B[jj + 4 + (3 - j)][ii + i];
+                            B[jj + j][ii + 4 + i] = a1;
+                            B[jj + 4 + (3 - j)][ii + i] = a0;
+                        }
+                    }
+                }
+                else
+                {
+                    j = jj;
+                    for (i = ii; i < ii + 4; i++)
+                    {
+                        B[j + 0][i] = A[i][j + 0];
+                        B[j + 1][i] = A[i][j + 1];
+                        B[j + 2][i] = A[i][j + 2];
+                        B[j + 3][i] = A[i][j + 3];
+                        B[j + 3][i + 4] = A[i][j + 4];
+                        B[j + 2][i + 4] = A[i][j + 5];
+                        B[j + 1][i + 4] = A[i][j + 6];
+                        B[j + 0][i + 4] = A[i][j + 7];
+                        if (i == ii + 0)
+                        {
+                            a0 = B[j + 0][i + 4];
+                            a4 = B[j + 1][i + 4];
+                        }
+                        else if (i == ii + 1)
+                        {
+                            a1 = B[j + 0][i + 4];
+                            a5 = B[j + 1][i + 4];
+                        }
+                        else if (i == ii + 2)
+                        {
+                            a2 = B[j + 0][i + 4];
+                            a6 = B[j + 1][i + 4];
+                        }
+                        else if (i == ii + 3)
+                        {
+                            a3 = B[j + 0][i + 4];
+                            a7 = B[j + 1][i + 4];
+                        }
+                    }
+                    for (i = ii + 4; i < ii + 8; i++)
+                    {
+                        B[j + 7][i - 4] = A[i][j + 0];
+                        B[j + 6][i - 4] = A[i][j + 1];
+                        B[j + 5][i - 4] = A[i][j + 2];
+                        B[j + 4][i - 4] = A[i][j + 3];
+                        B[j + 4][i] = A[i][j + 4];
+                        B[j + 5][i] = A[i][j + 5];
+                        B[j + 6][i] = A[i][j + 6];
+                        B[j + 7][i] = A[i][j + 7];
+                    }
+
+                    B[jj + 7][ii + 0] ^= a0; a0 ^= B[jj + 7][ii + 0]; B[jj + 7][ii + 0] ^= a0;
+                    B[jj + 7][ii + 1] ^= a1; a1 ^= B[jj + 7][ii + 1]; B[jj + 7][ii + 1] ^= a1;
+                    B[jj + 7][ii + 2] ^= a2; a2 ^= B[jj + 7][ii + 2]; B[jj + 7][ii + 2] ^= a2;
+                    B[jj + 7][ii + 3] ^= a3; a3 ^= B[jj + 7][ii + 3]; B[jj + 7][ii + 3] ^= a3;
+
+                    B[jj + 3][ii + 4] ^= a0; a0 ^= B[jj + 3][ii + 4]; B[jj + 3][ii + 4] ^= a0;
+                    B[jj + 3][ii + 5] ^= a1; a1 ^= B[jj + 3][ii + 5]; B[jj + 3][ii + 5] ^= a1;
+                    B[jj + 3][ii + 6] ^= a2; a2 ^= B[jj + 3][ii + 6]; B[jj + 3][ii + 6] ^= a2;
+                    B[jj + 3][ii + 7] ^= a3; a3 ^= B[jj + 3][ii + 7]; B[jj + 3][ii + 7] ^= a3;
+
+                    B[jj + 4][ii + 0] ^= a0; a0 ^= B[jj + 4][ii + 0]; B[jj + 4][ii + 0] ^= a0;
+                    B[jj + 4][ii + 1] ^= a1; a1 ^= B[jj + 4][ii + 1]; B[jj + 4][ii + 1] ^= a1;
+                    B[jj + 4][ii + 2] ^= a2; a2 ^= B[jj + 4][ii + 2]; B[jj + 4][ii + 2] ^= a2;
+                    B[jj + 4][ii + 3] ^= a3; a3 ^= B[jj + 4][ii + 3]; B[jj + 4][ii + 3] ^= a3;
+
+                    B[jj + 0][ii + 4] ^= a0; a0 ^= B[jj + 0][ii + 4]; B[jj + 0][ii + 4] ^= a0;
+                    B[jj + 0][ii + 5] ^= a1; a1 ^= B[jj + 0][ii + 5]; B[jj + 0][ii + 5] ^= a1;
+                    B[jj + 0][ii + 6] ^= a2; a2 ^= B[jj + 0][ii + 6]; B[jj + 0][ii + 6] ^= a2;
+                    B[jj + 0][ii + 7] ^= a3; a3 ^= B[jj + 0][ii + 7]; B[jj + 0][ii + 7] ^= a3;
+
+                    B[jj + 0][ii + 4] ^= B[jj + 3][ii + 4];
+                    B[jj + 3][ii + 4] ^= B[jj + 0][ii + 4];
+                    B[jj + 0][ii + 4] ^= B[jj + 3][ii + 4];
+
+                    B[jj + 0][ii + 5] ^= B[jj + 3][ii + 5];
+                    B[jj + 3][ii + 5] ^= B[jj + 0][ii + 5];
+                    B[jj + 0][ii + 5] ^= B[jj + 3][ii + 5];
+
+                    B[jj + 0][ii + 6] ^= B[jj + 3][ii + 6];
+                    B[jj + 3][ii + 6] ^= B[jj + 0][ii + 6];
+                    B[jj + 0][ii + 6] ^= B[jj + 3][ii + 6];
+
+                    B[jj + 0][ii + 7] ^= B[jj + 3][ii + 7];
+                    B[jj + 3][ii + 7] ^= B[jj + 0][ii + 7];
+                    B[jj + 0][ii + 7] ^= B[jj + 3][ii + 7];
+
+                    B[jj + 6][ii + 0] ^= a4; a4 ^= B[jj + 6][ii + 0]; B[jj + 6][ii + 0] ^= a4;
+                    B[jj + 6][ii + 1] ^= a5; a5 ^= B[jj + 6][ii + 1]; B[jj + 6][ii + 1] ^= a5;
+                    B[jj + 6][ii + 2] ^= a6; a6 ^= B[jj + 6][ii + 2]; B[jj + 6][ii + 2] ^= a6;
+                    B[jj + 6][ii + 3] ^= a7; a7 ^= B[jj + 6][ii + 3]; B[jj + 6][ii + 3] ^= a7;
+
+                    B[jj + 2][ii + 4] ^= a4; a4 ^= B[jj + 2][ii + 4]; B[jj + 2][ii + 4] ^= a4;
+                    B[jj + 2][ii + 5] ^= a5; a5 ^= B[jj + 2][ii + 5]; B[jj + 2][ii + 5] ^= a5;
+                    B[jj + 2][ii + 6] ^= a6; a6 ^= B[jj + 2][ii + 6]; B[jj + 2][ii + 6] ^= a6;
+                    B[jj + 2][ii + 7] ^= a7; a7 ^= B[jj + 2][ii + 7]; B[jj + 2][ii + 7] ^= a7;
+
+                    B[jj + 5][ii + 0] ^= a4; a4 ^= B[jj + 5][ii + 0]; B[jj + 5][ii + 0] ^= a4;
+                    B[jj + 5][ii + 1] ^= a5; a5 ^= B[jj + 5][ii + 1]; B[jj + 5][ii + 1] ^= a5;
+                    B[jj + 5][ii + 2] ^= a6; a6 ^= B[jj + 5][ii + 2]; B[jj + 5][ii + 2] ^= a6;
+                    B[jj + 5][ii + 3] ^= a7; a7 ^= B[jj + 5][ii + 3]; B[jj + 5][ii + 3] ^= a7;
+
+                    B[jj + 1][ii + 4] ^= a4; a4 ^= B[jj + 1][ii + 4]; B[jj + 1][ii + 4] ^= a4;
+                    B[jj + 1][ii + 5] ^= a5; a5 ^= B[jj + 1][ii + 5]; B[jj + 1][ii + 5] ^= a5;
+                    B[jj + 1][ii + 6] ^= a6; a6 ^= B[jj + 1][ii + 6]; B[jj + 1][ii + 6] ^= a6;
+                    B[jj + 1][ii + 7] ^= a7; a7 ^= B[jj + 1][ii + 7]; B[jj + 1][ii + 7] ^= a7;
+
+                    B[jj + 1][ii + 4] ^= B[jj + 2][ii + 4];
+                    B[jj + 2][ii + 4] ^= B[jj + 1][ii + 4];
+                    B[jj + 1][ii + 4] ^= B[jj + 2][ii + 4];
+
+                    B[jj + 1][ii + 5] ^= B[jj + 2][ii + 5];
+                    B[jj + 2][ii + 5] ^= B[jj + 1][ii + 5];
+                    B[jj + 1][ii + 5] ^= B[jj + 2][ii + 5];
+
+                    B[jj + 1][ii + 6] ^= B[jj + 2][ii + 6];
+                    B[jj + 2][ii + 6] ^= B[jj + 1][ii + 6];
+                    B[jj + 1][ii + 6] ^= B[jj + 2][ii + 6];
+
+                    B[jj + 1][ii + 7] ^= B[jj + 2][ii + 7];
+                    B[jj + 2][ii + 7] ^= B[jj + 1][ii + 7];
+                    B[jj + 1][ii + 7] ^= B[jj + 2][ii + 7];
+
                 }
             }
         }
