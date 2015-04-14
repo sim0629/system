96cdfe81b42681ca34dfb315e934766375c7fd9e
Gyumin Sim <sim0629@gmail.com>
2015-04-14 18:43:23 +0900

    init

---
+++ config.h
@@ -0,0 +1,19 @@
+/*********************************************************
+ * config.h - Configuration data for the driver.c program.
+ *********************************************************/
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+/* 
+ * CPEs for the baseline (naive) version of the rotate function that
+ * was handed out to the students. Rd is the measured CPE for a dxd
+ * image. Run the driver.c program on your system to get these
+ * numbers.  
+ */
+#define R64    14.7
+#define R128   40.1
+#define R256   46.4
+#define R512   65.9
+#define R1024  94.5
+
+#endif /* _CONFIG_H_ */
