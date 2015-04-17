ba1057e5541d76be8338d3a014019304926f8010
Gyumin Sim <sim0629@gmail.com>
2015-04-17 17:59:22 +0900

    (n, m) in [8, 16, 32] x [8, 16, 32]

--- kernels.c
+++ kernels.c
@@ -43,7 +43,131 @@ void naive_rotate(int dim, pixel *src, pixel *dst)
 char rotate_descr[] = "rotate: Current working version";
 void rotate(int dim, pixel *src, pixel *dst)
 {
-    naive_rotate(dim, src, dst);
+    int ii, jj;
+    int i, j;
+    int n = 16, m = 8;
+    for (ii = 0; ii < dim; ii += n)
+    for (jj = 0; jj < dim; jj += m)
+        for (i = ii; i < ii + n; i++)
+        for (j = jj; j < jj + m; j++)
+            dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];
+}
+
+char rotate_8_8_descr[] = "rotate: 8 x 8";
+void rotate_8_8(int dim, pixel *src, pixel *dst)
+{
+    int ii, jj;
+    int i, j;
+    int n = 8, m = 8;
+    for (ii = 0; ii < dim; ii += n)
+    for (jj = 0; jj < dim; jj += m)
+        for (i = ii; i < ii + n; i++)
+        for (j = jj; j < jj + m; j++)
+            dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];
+}
+
+char rotate_8_16_descr[] = "rotate: 8 x 16";
+void rotate_8_16(int dim, pixel *src, pixel *dst)
+{
+    int ii, jj;
+    int i, j;
+    int n = 8, m = 16;
+    for (ii = 0; ii < dim; ii += n)
+    for (jj = 0; jj < dim; jj += m)
+        for (i = ii; i < ii + n; i++)
+        for (j = jj; j < jj + m; j++)
+            dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];
+}
+
+char rotate_8_32_descr[] = "rotate: 8 x 32";
+void rotate_8_32(int dim, pixel *src, pixel *dst)
+{
+    int ii, jj;
+    int i, j;
+    int n = 8, m = 32;
+    for (ii = 0; ii < dim; ii += n)
+    for (jj = 0; jj < dim; jj += m)
+        for (i = ii; i < ii + n; i++)
+        for (j = jj; j < jj + m; j++)
+            dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];
+}
+
+char rotate_16_8_descr[] = "rotate: 16 x 8";
+void rotate_16_8(int dim, pixel *src, pixel *dst)
+{
+    int ii, jj;
+    int i, j;
+    int n = 16, m = 8;
+    for (ii = 0; ii < dim; ii += n)
+    for (jj = 0; jj < dim; jj += m)
+        for (i = ii; i < ii + n; i++)
+        for (j = jj; j < jj + m; j++)
+            dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];
+}
+
+char rotate_16_16_descr[] = "rotate: 16 x 16";
+void rotate_16_16(int dim, pixel *src, pixel *dst)
+{
+    int ii, jj;
+    int i, j;
+    int n = 16, m = 16;
+    for (ii = 0; ii < dim; ii += n)
+    for (jj = 0; jj < dim; jj += m)
+        for (i = ii; i < ii + n; i++)
+        for (j = jj; j < jj + m; j++)
+            dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];
+}
+
+char rotate_16_32_descr[] = "rotate: 16 x 32";
+void rotate_16_32(int dim, pixel *src, pixel *dst)
+{
+    int ii, jj;
+    int i, j;
+    int n = 16, m = 32;
+    for (ii = 0; ii < dim; ii += n)
+    for (jj = 0; jj < dim; jj += m)
+        for (i = ii; i < ii + n; i++)
+        for (j = jj; j < jj + m; j++)
+            dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];
+}
+
+char rotate_32_8_descr[] = "rotate: 32 x 8";
+void rotate_32_8(int dim, pixel *src, pixel *dst)
+{
+    int ii, jj;
+    int i, j;
+    int n = 32, m = 8;
+    for (ii = 0; ii < dim; ii += n)
+    for (jj = 0; jj < dim; jj += m)
+        for (i = ii; i < ii + n; i++)
+        for (j = jj; j < jj + m; j++)
+            dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];
+}
+
+char rotate_32_16_descr[] = "rotate: 32 x 16";
+void rotate_32_16(int dim, pixel *src, pixel *dst)
+{
+    int ii, jj;
+    int i, j;
+    int n = 32, m = 16;
+    for (ii = 0; ii < dim; ii += n)
+    for (jj = 0; jj < dim; jj += m)
+        for (i = ii; i < ii + n; i++)
+        for (j = jj; j < jj + m; j++)
+            dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];
+}
+
+char rotate_32_32_descr[] = "rotate: 32 x 32";
+void rotate_32_32(int dim, pixel *src, pixel *dst)
+{
+    int ii, jj;
+    int i, j;
+    int n = 32, m = 32;
+    for (ii = 0; ii < dim; ii += n)
+    for (jj = 0; jj < dim; jj += m)
+        for (i = ii; i < ii + n; i++)
+        for (j = jj; j < jj + m; j++)
+            dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];
 }
 
 /*********************************************************************
@@ -58,5 +182,14 @@ void register_rotate_functions()
 {
     add_rotate_function(&naive_rotate, naive_rotate_descr);
     add_rotate_function(&rotate, rotate_descr);
+    add_rotate_function(&rotate_8_8, rotate_8_8_descr);
+    add_rotate_function(&rotate_8_16, rotate_8_16_descr);
+    add_rotate_function(&rotate_8_32, rotate_8_32_descr);
+    add_rotate_function(&rotate_16_8, rotate_16_8_descr);
+    add_rotate_function(&rotate_16_16, rotate_16_16_descr);
+    add_rotate_function(&rotate_16_32, rotate_16_32_descr);
+    add_rotate_function(&rotate_32_8, rotate_32_8_descr);
+    add_rotate_function(&rotate_32_16, rotate_32_16_descr);
+    add_rotate_function(&rotate_32_32, rotate_32_32_descr);
     /* ... Register additional test functions here */
 }
