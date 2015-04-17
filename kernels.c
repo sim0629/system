de17cc62b1ec7341b85c5ad2b8c323a7f3c7e9f3
Gyumin Sim <sim0629@gmail.com>
2015-04-17 17:45:56 +0900

    remove trailing spaces

--- kernels.c
+++ kernels.c
@@ -6,8 +6,8 @@
 #include <stdlib.h>
 #include "defs.h"
 
-/* 
- * Please fill in the following student struct 
+/*
+ * Please fill in the following student struct
  */
 student_t student = {
     "2009-11744",              /* Team name */
@@ -23,11 +23,11 @@ student_t student = {
  * Your different versions of the rotate kernel go here
  ******************************************************/
 
-/* 
- * naive_rotate - The naive baseline version of rotate 
+/*
+ * naive_rotate - The naive baseline version of rotate
  */
 char naive_rotate_descr[] = "naive_rotate: Naive baseline implementation";
-void naive_rotate(int dim, pixel *src, pixel *dst) 
+void naive_rotate(int dim, pixel *src, pixel *dst)
 {
     int i, j;
 
@@ -36,12 +36,12 @@ void naive_rotate(int dim, pixel *src, pixel *dst)
 	    dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];
 }
 
-/* 
+/*
  * rotate - Your current working version of rotate
  * IMPORTANT: This is the version you will be graded on
  */
 char rotate_descr[] = "rotate: Current working version";
-void rotate(int dim, pixel *src, pixel *dst) 
+void rotate(int dim, pixel *src, pixel *dst)
 {
     naive_rotate(dim, src, dst);
 }
@@ -51,12 +51,12 @@ void rotate(int dim, pixel *src, pixel *dst)
  *     of the rotate kernel with the driver by calling the
  *     add_rotate_function() for each test function. When you run the
  *     driver program, it will test and report the performance of each
- *     registered test function.  
+ *     registered test function.
  *********************************************************************/
 
-void register_rotate_functions() 
+void register_rotate_functions()
 {
-    add_rotate_function(&naive_rotate, naive_rotate_descr);   
-    add_rotate_function(&rotate, rotate_descr);   
+    add_rotate_function(&naive_rotate, naive_rotate_descr);
+    add_rotate_function(&rotate, rotate_descr);
     /* ... Register additional test functions here */
 }
