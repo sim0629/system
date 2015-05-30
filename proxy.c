f6a55f81b738f3be252b87b481e78b07cfebdaf3
Gyumin Sim <sim0629@gmail.com>
2015-05-31 05:27:08 +0900

    Implement proxy_parse_http_header

--- proxy.c
+++ proxy.c
@@ -11,12 +11,28 @@
 
 #include "csapp.h"
 
+#define CHUNKED -1
+
 struct conn_info
 {
     int fd;
     rio_t rio;
     struct sockaddr_in addr;
     socklen_t addr_len;
+};
+
+struct http_header
+{
+    char first_line[MAXLINE];
+    char buf[MAXBUF];
+    int buf_length;
+    int content_length;
+
+    char *method;
+    char *host;
+    char *port;
+    char *path;
+    char *version;
 };
 
 /*
@@ -36,6 +52,96 @@ static int proxy_accept(int listen_fd, struct conn_info *conn_info_ptr)
     return 0;
 }
 
+static int proxy_parse_http_header(rio_t *rio_ptr, struct http_header *header_ptr)
+{
+    char line[MAXLINE];
+    int line_len;
+
+    if ((line_len = rio_readlineb(rio_ptr, header_ptr->first_line, MAXLINE)) <= 0)
+        return -1;
+    header_ptr->first_line[line_len] = '\0';
+
+    header_ptr->buf_length = 0;
+    header_ptr->content_length = 0;
+
+    while ((line_len = rio_readlineb(rio_ptr, line, MAXLINE)) > 0) {
+        memcpy(header_ptr->buf + header_ptr->buf_length, line, line_len);
+        header_ptr->buf_length += line_len;
+
+        if (header_ptr->content_length == 0) {
+            const char *te = "Transfer-Encoding: ";
+            const char *cl = "Content-Length: ";
+            if (strncasecmp(line, te, strlen(te)) == 0) {
+                if (strncmp(line + strlen(te), "indentity", 9) != 0)
+                    header_ptr->content_length = CHUNKED;
+            }
+            else if (strncasecmp(line, cl, strlen(cl)) == 0) {
+                header_ptr->content_length = atoi(line + strlen(cl));
+            }
+        }
+
+        if (strncmp(line, "\r\n", 2) == 0)
+            return 0;
+    }
+
+    return -1;
+}
+
+static int proxy_parse_request_header(rio_t *rio_ptr, struct http_header *header_ptr)
+{
+    if (proxy_parse_http_header(rio_ptr, header_ptr) < 0)
+        return -1;
+
+    header_ptr->method = header_ptr->first_line;
+    header_ptr->host = strchr(header_ptr->method, ' ');
+    if (header_ptr->host == NULL)
+        return -1;
+    header_ptr->host[0] = '\0';
+    header_ptr->host++;
+    if (strncmp(header_ptr->host, "http://", 7) != 0)
+        return -1;
+    header_ptr->host += 7;
+    header_ptr->port = strpbrk(header_ptr->host, ":/");
+    if (header_ptr->port == NULL)
+        return -1;
+    if (header_ptr->port[0] == ':') {
+        header_ptr->port[0] = '\0';
+        header_ptr->port++;
+        header_ptr->path = strchr(header_ptr->port, '/');
+        if (header_ptr->path == NULL)
+            return -1;
+    }
+    else {
+        header_ptr->path = header_ptr->port;
+        header_ptr->port = NULL;
+    }
+    header_ptr->path[0] = '\0';
+    header_ptr->path++;
+    header_ptr->version = strchr(header_ptr->path, ' ');
+    if (header_ptr->version == NULL)
+        return -1;
+    header_ptr->version[0] = '\0';
+    header_ptr->version++;
+
+    return 0;
+}
+
+static int proxy_parse_response_header(rio_t *rio_ptr, struct http_header *header_ptr)
+{
+    return proxy_parse_http_header(rio_ptr, header_ptr);
+}
+
+static void proxy_relay(struct conn_info *conn_info_ptr)
+{
+    struct http_header request_header;
+
+    if (proxy_parse_request_header(&conn_info_ptr->rio, &request_header) < 0) {
+        close(conn_info_ptr->fd);
+        return;
+    }
+    // TODO
+}
+
 static void proxy_listen_forever(int port)
 {
     int listen_fd;
@@ -49,7 +155,7 @@ static void proxy_listen_forever(int port)
         struct conn_info conn_info;
         if (proxy_accept(listen_fd, &conn_info) < 0)
             continue;
-        // TODO
+        proxy_relay(&conn_info);
     }
 }
 
