66abc4c71b1682898970eec47d86b83a5aef4c49
Gyumin Sim <sim0629@gmail.com>
2015-06-02 21:59:50 +0900

    Add comments and break long line

--- proxy.c
+++ proxy.c
@@ -4,16 +4,29 @@
  * Student Information:
  * 심규민, 2009-11744
  *
- * IMPORTANT: Give a high level description of your code here. You
- * must also provide a header comment at the beginning of each
- * function that describes what that function does.
+ *  1. 주어진 command line argument로부터 port 번호를 파싱하고 listen 합니다.
+ *  2. 브라우져를 accept 하여 그 정보를 conn_info에 저장합니다.
+ *  3. 브라우져의 요청을 빈 줄이 나올 때까지 한 줄씩 읽으며 파싱하여
+ *     header_info에 저장합니다.
+ *  4. 요청 맨 첫 줄을 파싱하여 웹서버의 주소를 알아냅니다.
+ *  5. 웹서버에 connect 하여 그 정보를 client_info에 저장합니다.
+ *  6. 요청 header를 웹서버에 전달합니다.
+ *  7. 요청 header에서 chunked encoding인지 아닌지에 따라 content length만큼
+ *     body를 읽어서 웹서버에 전달합니다.
+ *  8. 웹서버의 응답을 빈 줄이 나올 때까지 한 줄씩 읽으며 파싱하여
+ *     header_info에 저장합니다.
+ *  9. 응답 header를 브라우져에 전달합니다.
+ * 10. 응답 header에서 chunked encoding인지 아닌지에 따라 content length만큼
+ *     body를 읽어서 브라우져에 전달합니다.
+ * 11. 양쪽의 연결을 끊고 2.부터 반복합니다.
  */
 
 #include "csapp.h"
 
-#define CHUNKED -1
+#define CHUNKED -1 // chunked encoding일 때 http_header의 content_length 값
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
 
+/* 브라우져와 연결된 socket 정보 */
 struct conn_info
 {
     int fd;
@@ -22,12 +35,17 @@ struct conn_info
     socklen_t addr_len;
 };
 
+/* 웹서버와 연결된 socket 정보 */
 struct client_info
 {
     int fd;
     rio_t rio;
 };
 
+/*
+ * http header 정보
+ * request와 response를 겸합니다.
+ */
 struct http_header
 {
     char first_line[MAXLINE];
@@ -35,6 +53,7 @@ struct http_header
     int buf_length;
     int content_length;
 
+    /* 아래는 request에서만 쓰입니다. */
     char *method;
     char *host;
     char *port;
@@ -47,10 +66,15 @@ struct http_header
  */
 static void print_log_entry(struct sockaddr_in *sockaddr, char *uri, int size);
 
+/*
+ * listen_fd에서 socket을 accept 하여 conn_info를 채웁니다.
+ * 성공할 경우 0을, 실패할 경우 -1을 리턴합니다.
+ */
 static int proxy_accept(int listen_fd, struct conn_info *conn_info_ptr)
 {
     conn_info_ptr->addr_len = sizeof(conn_info_ptr->addr);
-    conn_info_ptr->fd = accept(listen_fd, (SA *)&conn_info_ptr->addr, &conn_info_ptr->addr_len);
+    conn_info_ptr->fd = accept(listen_fd, (SA *)&conn_info_ptr->addr,
+                               &conn_info_ptr->addr_len);
     if (conn_info_ptr->fd < 0) {
         fprintf(stderr, "Fail to accept\n");
         return -1;
@@ -59,12 +83,18 @@ static int proxy_accept(int listen_fd, struct conn_info *conn_info_ptr)
     return 0;
 }
 
-static int proxy_parse_http_header(rio_t *rio_ptr, struct http_header *header_ptr)
+/*
+ * rio buffer에서 http header를 읽고 파싱하여 채웁니다.
+ * 성공할 경우 0을, 실패할 경우 -1을 리턴합니다.
+ */
+static int proxy_parse_http_header(rio_t *rio_ptr,
+                                   struct http_header *header_ptr)
 {
     char line[MAXLINE];
     int line_len;
 
-    if ((line_len = rio_readlineb(rio_ptr, header_ptr->first_line, MAXLINE)) <= 0)
+    if ((line_len = rio_readlineb(rio_ptr, header_ptr->first_line, MAXLINE))
+        <= 0)
         return -1;
     header_ptr->first_line[line_len] = '\0';
 
@@ -94,7 +124,13 @@ static int proxy_parse_http_header(rio_t *rio_ptr, struct http_header *header_pt
     return -1;
 }
 
-static int proxy_parse_request_header(rio_t *rio_ptr, struct http_header *header_ptr)
+/*
+ * rio buffer에서 http request header를 읽고 파싱하여 채웁니다.
+ * 공통 header를 파싱하고, 맨 첫 줄을 파싱합니다.
+ * 성공할 경우 0을, 실패할 경우 -1을 리턴합니다.
+ */
+static int proxy_parse_request_header(rio_t *rio_ptr,
+                                      struct http_header *header_ptr)
 {
     if (proxy_parse_http_header(rio_ptr, header_ptr) < 0)
         return -1;
@@ -133,12 +169,23 @@ static int proxy_parse_request_header(rio_t *rio_ptr, struct http_header *header
     return 0;
 }
 
-static int proxy_parse_response_header(rio_t *rio_ptr, struct http_header *header_ptr)
+/*
+ * rio buffer에서 http response header를 읽고 파싱하여 채웁니다.
+ * 공통 header를 파싱하고, 특별한 작업을 더 하지 않습니다.
+ * 성공할 경우 0을, 실패할 경우 -1을 리턴합니다.
+ */
+static int proxy_parse_response_header(rio_t *rio_ptr,
+                                       struct http_header *header_ptr)
 {
     return proxy_parse_http_header(rio_ptr, header_ptr);
 }
 
-static int proxy_connect(struct client_info *client_info_ptr, struct http_header *header_ptr)
+/*
+ * request header 정보를 토대로 웹서버에 connect 하고 client_info를 채웁니다.
+ * 성공할 경우 0을, 실패할 경우 -1을 리턴합니다.
+ */
+static int proxy_connect(struct client_info *client_info_ptr,
+                         struct http_header *header_ptr)
 {
     int port = 80;
 
@@ -153,6 +200,10 @@ static int proxy_connect(struct client_info *client_info_ptr, struct http_header
     return 0;
 }
 
+/*
+ * rio buffer에서 size만큼 읽어서 to socket에 씁니다.
+ * 성공할 경우 0을, 실패할 경우 -1을 리턴합니다.
+ */
 static int proxy_toss(rio_t *from_rio_ptr, int to_fd, int size)
 {
     char line[MAXLINE];
@@ -170,6 +221,11 @@ static int proxy_toss(rio_t *from_rio_ptr, int to_fd, int size)
     return 0;
 }
 
+/*
+ * 브라우져의 요청을 받아 웹서버에 전달하고
+ * 웹서버의 응답을 받아 브라우져에 전달합니다.
+ * 하나의 요청-응답만을 처리하고 log를 남긴 후에 양쪽의 연결의 모두 끊습니다.
+ */
 static void proxy_relay(struct conn_info *conn_info_ptr)
 {
     struct client_info client_info;
@@ -180,21 +236,28 @@ static void proxy_relay(struct conn_info *conn_info_ptr)
     int line_len;
     int recv_size = 0;
 
+    // 요청 header 파싱
     if (proxy_parse_request_header(&conn_info_ptr->rio, &request_header) < 0) {
         close(conn_info_ptr->fd);
         return;
     }
 
+    // 웹서버에 연결
     if (proxy_connect(&client_info, &request_header) < 0) {
         close(conn_info_ptr->fd);
         return;
     }
 
-    line_len = sprintf(line, "%s /%s %s", request_header.method, request_header.path, request_header.version);
+    // 웹서버에 header 전달
+    line_len = sprintf(line, "%s /%s %s", request_header.method,
+                       request_header.path, request_header.version);
     if (rio_writen(client_info.fd, line, line_len) <= 0)
         goto proxy_relay_end;
-    if (rio_writen(client_info.fd, request_header.buf, request_header.buf_length) <= 0)
+    if (rio_writen(client_info.fd, request_header.buf,
+                   request_header.buf_length) <= 0)
         goto proxy_relay_end;
+
+    // 요청 body를 읽어서 웹서버에 전달
     if (request_header.content_length == CHUNKED) {
         while (1) {
             line_len = rio_readlineb(&conn_info_ptr->rio, line, MAXLINE);
@@ -209,20 +272,27 @@ static void proxy_relay(struct conn_info *conn_info_ptr)
                 break;
         }
     }else if (request_header.content_length > 0) {
-        if (proxy_toss(&conn_info_ptr->rio, client_info.fd, request_header.content_length) < 0)
+        if (proxy_toss(&conn_info_ptr->rio, client_info.fd,
+                       request_header.content_length) < 0)
             goto proxy_relay_end;
     }
 
+    // 응답 header 파싱
     if (proxy_parse_response_header(&client_info.rio, &response_header) < 0)
         goto proxy_relay_end;
 
+    // 브라우져에 header 전달
     line_len = strlen(response_header.first_line);
     recv_size += line_len;
-    if (rio_writen(conn_info_ptr->fd, response_header.first_line, line_len) <= 0)
+    if (rio_writen(conn_info_ptr->fd, response_header.first_line, line_len)
+        <= 0)
         goto proxy_relay_end;
     recv_size += response_header.buf_length;
-    if (rio_writen(conn_info_ptr->fd, response_header.buf, response_header.buf_length) <= 0)
+    if (rio_writen(conn_info_ptr->fd, response_header.buf,
+                   response_header.buf_length) <= 0)
         goto proxy_relay_end;
+
+    // 응답 body를 읽어서 브라우져에 전달
     if (response_header.content_length == CHUNKED) {
         while (1) {
             line_len = rio_readlineb(&client_info.rio, line, MAXLINE);
@@ -240,18 +310,25 @@ static void proxy_relay(struct conn_info *conn_info_ptr)
         }
     }else if (response_header.content_length > 0) {
         line_len += request_header.content_length;
-        if (proxy_toss(&client_info.rio, conn_info_ptr->fd, response_header.content_length) < 0)
+        if (proxy_toss(&client_info.rio, conn_info_ptr->fd,
+                       response_header.content_length) < 0)
             goto proxy_relay_end;
     }
 
+    // 로그 출력
     sprintf(line, "http://%s/%s", request_header.host, request_header.path);
     print_log_entry(&conn_info_ptr->addr, line, recv_size);
 
 proxy_relay_end:
+    // 연결 끊기
     close(client_info.fd);
     close(conn_info_ptr->fd);
 }
 
+/*
+ * 주어진 port에서 무한히 listen 하며 accept, relay 합니다.
+ * listen에 실패하면 프로세스를 종료합니다.
+ */
 static void proxy_listen_forever(int port)
 {
     int listen_fd;
@@ -280,6 +357,7 @@ int main(int argc, char **argv)
         exit(0);
     }
 
+    // pipe signal 무시
     signal(SIGPIPE, SIG_IGN);
 
     proxy_listen_forever(atoi(argv[1]));
